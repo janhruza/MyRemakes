98601e9881a192c8a969124ead8cee8d753ff97a
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-06-02 23:52:25 +0200

    Resource name changes.

--- ATM/res/resource.h
+++ ATM/res/resource.h
@@ -11,6 +11,7 @@
 #define IDC_TXT_USERNAME                1009
 #define IDC_BALANCE                     1012
 #define SS_ETCHEDHORZ                   1013
+#define IDC_CHECK1                      1014
 
 // Next default values for new objects
 // 
@@ -18,7 +19,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
