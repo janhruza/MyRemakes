7d023d3db50fd038f1901127baea0d4f7d25e01b
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-06-20 14:22:00 +0200

    UX improvements.

--- Warden/src/TrayIcon.c
+++ Warden/src/TrayIcon.c
@@ -9,10 +9,15 @@ void Tray_AddIcon(HWND hDlg)
     nid.cbSize = sizeof(NOTIFYICONDATAW);
     nid.hWnd = hDlg;
     nid.uID = 1;
-    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO;
     nid.uCallbackMessage = WM_TRAYICON;
     nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    StringCchCopy(nid.szTip, 128, TEXT("Warden - Session Active"));
+    StringCchCopyW(nid.szTip, 128, L"Warden - Session Active");
+
+    // notify icon
+    StringCchCopyW(nid.szInfo, 256, L"Warden is active and monitoring your workspace. Click this icon to end your session.");
+    StringCchCopyW(nid.szInfoTitle, 64, L"Session Started");
+    nid.dwInfoFlags = NIIF_INFO;
 
     Shell_NotifyIconW(NIM_ADD, &nid);
 }
